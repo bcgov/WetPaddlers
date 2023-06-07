c1c02beacae7fabc9bebd271320b10e76ab9e4a2
Conor Brady <con.brad@gmail.com>
2023-06-07 14:50:51 -0700

    tippecanoe subprocess command

--- backend/api/tippecanoe.py
+++ backend/api/tippecanoe.py
@@ -1,3 +1,4 @@
+import subprocess
 from pydantic_geojson import FeatureCollectionModel
 
 
@@ -6,4 +7,19 @@ def process_geojson(feature_collection: FeatureCollectionModel):
         with open("tippecanoe_input.json", "w") as f:
             f.write(feature_collection.json())
     except Exception as e:
+        print(e)
+    try:
+        subprocess.run(
+            [
+                "tippecanoe",
+                "-zg",
+                "--projection=EPSG:4326",
+                "tippecanoe_input.json",
+                "--output=tippecanoe_output.pmtiles",
+                "--no-tile-compression",
+            ],
+            check=True,
+        )
+    except Exception as e:
+        print("error creating pm tiles")
         print(e)
