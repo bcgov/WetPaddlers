bb9789f895d7f503acd60d9b10749efa3239adce
Conor Brady <con.brad@gmail.com>
2023-06-07 10:24:18 -0700

    Placeholder the openlayers component

--- wetpaddle/src/App.tsx
+++ wetpaddle/src/App.tsx
@@ -1,10 +1,15 @@
+import './App.css'
 
+import { ReactMap } from './Map'
 import { OpenLayersMap } from './openlayers/OpenLayersMap'
 
 function App() {
 
   return (
-    <OpenLayersMap />
+    <div className="app">
+    <ReactMap/>
+    {/*<OpenLayersMap />  <-- TODO, openlayers map component*/}
+    </div>
   )
 }
 
