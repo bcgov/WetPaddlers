f46b0491f0f3b72dd7366f1b7fa40331639bbf8a
Darren Boss <darren.boss@gov.bc.ca>
2023-06-07 15:16:27 -0700

    Initial front-end converter component Some CORS stuff

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,11 +2,20 @@ import logging
 from fastapi import FastAPI, Response
 from pydantic_geojson import FeatureCollectionModel
 from . import tippecanoe
+from fastapi.middleware.cors import CORSMiddleware
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='*',
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
