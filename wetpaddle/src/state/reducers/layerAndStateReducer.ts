c8592cd555919a9081d91d3741b7e31efd0e7b63
Micheal Wells <micheal.w.wells@gmail.com>
2023-06-08 09:09:48 -0700

    can grab layer list from url

--- wetpaddle/src/state/reducers/layerAndStateReducer.ts
+++ wetpaddle/src/state/reducers/layerAndStateReducer.ts
@@ -1,5 +1,13 @@
 import { URL_CHANGE } from "../actions";
 
+// http://localhost:5173/?layers=[{%20%22name%22:%20%22example_name%22,%20%22s3_url%22:%20%22https://nrs.objectstore.gov.bc.ca/uphjps/riso.pmtiles%22,%20%22shape_paint_rules_list_id%22:%201,%20%22label_property%22:%20%22code_name%22%20}]
+
+interface Layer {
+    name: string
+    s3_url: string
+    shape_pain_rules_list_id: number 
+    label_property: string
+}
 
 const initialState: { url: string } = {
   url: ''
@@ -10,7 +18,8 @@ export default function layerAndURLStateReducer(state = initialState, action: an
     case URL_CHANGE.toString():
       return {
         ...state,
-        url: action.payload.url
+        url: action.payload.url,
+        layers: JSON.parse(action.payload.layers) as Layer[]
       }
     default:
       return state;
